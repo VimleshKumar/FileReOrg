8f674c202b1f4ea1cd7a6c3ad88ee9fb5942bb19
Vimlesh Kumar <vimlesh1@gmail.com>
2020-05-28 19:58:44 +0530

    Initiate Projects Structure
    Create initial class
    update copyright license
    
    Signed-off-by: Vimlesh Kumar <vimlesh1@gmail.com>

---
+++ src/services/FileOrganise.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2020.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package services;
+
+/**
+ * {@link FileOrganise}
+ *
+ * Provide methods for file compression and decompression.<br><br>
+ *
+ * Use appropriate library.<br><br>
+ *
+ * Should be able to support zip, 7z and rar formats.
+ *
+ * @version 1.0.0
+ * @see ui.FROAppController
+ */
+
+public class FileOrganise {
+
+
+
+}
